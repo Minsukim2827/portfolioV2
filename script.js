0e0c68b94bd25844ae539ab8a9fc1984ca1bd9b8
Minsu Kim <m.kim006064@gmaiil.com>
2023-09-23 15:08:45 +1200

    changed resume, changed projects

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ function scrollToSection(section) {
     const container = document.querySelector(section);
     container.scrollIntoView({ behavior: "smooth" });
 }
+
+document.getElementById("openPdfBtn").addEventListener("click", function () {
+    document
+        .getElementById("pdfEmbed")
+        .setAttribute("src", "resume/resume.pdf");
+    document.getElementById("pdfModal").style.display = "block";
+});
+
+document.getElementById("closePdfBtn").addEventListener("click", function () {
+    document.getElementById("pdfModal").style.display = "none";
+});
+
+// Close modal by clicking outside
+document.getElementById("pdfModal").addEventListener("click", function (event) {
+    if (event.target === this) {
+        this.style.display = "none";
+    }
+});
